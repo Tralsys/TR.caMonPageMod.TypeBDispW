554e1129a0387419619dd715ac381df988cc3c51
ＴＲ <public@technotter.com>
2021-04-16 02:15:50 +0900

    倍率器コンバータを追加

---
+++ TR.caMonPageMod.TypeBDispW/ValueConverters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TR.caMonPageMod.TypeBDispW
+{
+	public class ValueMultiplConverter : IValueConverter
+	{
+		public double MultiplValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (double)value * MultiplValue;
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (double)value / MultiplValue;
+	}
+}
