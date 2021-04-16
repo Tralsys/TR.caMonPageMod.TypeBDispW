aaf598d45e279722e1ebe88251d62a8a089af71e
ＴＲ <public@technotter.com>
2021-04-16 13:21:40 +0900

    Update ValueConverters.cs
    - TakeSelectedSideThicknessConverter
    - DoubleToThicknessConverter
    - ValueConverterGroup
    以上の2コンバータを追加

--- TR.caMonPageMod.TypeBDispW/ValueConverters.cs
+++ TR.caMonPageMod.TypeBDispW/ValueConverters.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace TR.caMonPageMod.TypeBDispW
 {
+	[ValueConversion(typeof(double), typeof(double))]
 	public class ValueMultiplConverter : IValueConverter
 	{
 		public double MultiplValue { get; set; }
@@ -11,5 +15,68 @@ namespace TR.caMonPageMod.TypeBDispW
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (double)value * MultiplValue;
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (double)value / MultiplValue;
+	}
+
+	[ValueConversion(typeof(Thickness), typeof(Thickness))]
+	public class TakeSelectedSideThicknessConverter : IValueConverter
+	{
+		[Flags]
+		public enum Side
+		{
+			None = 0,
+			Left = 0b000001,
+			Top = 0b00000010,
+			Right = 0b00100,
+			Bottom = 0b1000
+		}
+
+		public Side SideSelect { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value is Thickness t ?
+				new Thickness(
+					SideSelect.HasFlag(Side.Left) ? t.Left : 0,
+					SideSelect.HasFlag(Side.Top) ? t.Top : 0,
+					SideSelect.HasFlag(Side.Right) ? t.Right : 0,
+					SideSelect.HasFlag(Side.Bottom) ? t.Bottom : 0
+					)
+			: new Thickness(0);
+
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+
+	[ValueConversion(typeof(double), typeof(Thickness))]
+	public class DoubleToThicknessConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => new Thickness((double)value);
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => ((Thickness)value).Left;
+	}
+
+	//ref : https://oita.oika.me/2018/04/15/pilevalueconverter/
+	[ContentProperty(nameof(Converters))]
+	public class ValueConverterGroup : IValueConverter
+	{
+		public List<IValueConverter> Converters { get; } = new();
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (Converters.Count <= 0)
+				return value;
+			foreach (var cnv in Converters)
+				value = cnv.Convert(value, targetType, parameter, culture);
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (Converters.Count <= 0)
+				return value;
+			foreach (var cnv in Converters)
+				value = cnv.ConvertBack(value, targetType, parameter, culture);
+			return value;
+		}
 	}
 }
